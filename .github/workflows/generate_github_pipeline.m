549bed4c200386bed6a2a8fd6a9c7bb59c3b4b4c
Ahmed Hassan <ahmedh@mathworks.com>
2025-06-24 22:48:08 -0400

    generate_github_pipeline

--- .github/workflows/generate_github_pipeline.m
+++ .github/workflows/generate_github_pipeline.m
@@ -1,22 +1,30 @@
 % Copyright 2025 The MathWorks, Inc.
 
-op = padv.pipeline.GitHubOptions;
-op.PipelineArchitecture = "IndependentModelPipelines";
-op.GeneratorVersion = 2;
-op.MatlabInstallationLocation = "C:/Program Files/MATLAB/R2024b/bin";
-op.RunnerLabels = "padv_win_agents";
-op.GeneratedPipelineDirectory = "_build_";
-op.StopOnStageFailure = true;
-op.RunprocessCommandOptions.GenerateJUnitForProcess = true;
-op.ReportPath = "$PROJECTROOT$/PA_Results/Report/PadvReport";
-op.ProjectToRepoPath = "";
-op.UseMatlabPlugin = false;
-
-% Docker image settings
-op.UseMatlabPlugin = false;
-% examples: "matlab", "matlab-batch", "xvfb-run -a matlab", "xvfb-run -a matlab-batch"
-op.MatlabLaunchCmd = "xvfb-run -a matlab-batch";
-op.MatlabStartupOptions = "";
-op.AddBatchStartupOption = false;
-padv.pipeline.generatePipeline(op, "CIPipeline");
-
+function generate_github_pipeline(workspace, projectToRepoPath, matlabInstallationLocation, runnerLabels, build_folder)
+    arguments
+        workspace = pwd;
+        projectToRepoPath = "";
+        matlabInstallationLocation = "matlab_bin_path";
+        runnerLabels = "github_runner_labels";
+        build_folder = "_build_"
+    end
+    cp = openProject(strjoin({workspace,projectToRepoPath} , filesep));
+    op = padv.pipeline.GitHubOptions;
+    op.PipelineArchitecture = "IndependentModelPipelines";
+    op.GeneratorVersion = 2;
+    op.MatlabInstallationLocation = matlabInstallationLocation;     % "C:/Program Files/MATLAB/R2024b/bin";
+    op.RunnerLabels = runnerLabels;                                 % "padv_win_agents";
+    op.GeneratedPipelineDirectory = build_folder;                   % "_build_";
+    op.StopOnStageFailure = true;
+    op.RunprocessCommandOptions.GenerateJUnitForProcess = true;
+    op.ReportPath = "$PROJECTROOT$/PA_Results/Report/PadvReport";
+    op.ProjectToRepoPath = projectToRepoPath;                       % ""
+    op.UseMatlabPlugin = false;
+    
+    % Docker image settings
+    % examples: "matlab", "matlab-batch", "xvfb-run -a matlab", "xvfb-run -a matlab-batch"
+    % op.MatlabLaunchCmd = "xvfb-run -a matlab-batch";
+    % op.MatlabStartupOptions = "";
+    % op.AddBatchStartupOption = false;
+    % padv.pipeline.generatePipeline(op, "CIPipeline");
+end
