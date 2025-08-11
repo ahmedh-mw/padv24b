9868d73dfbb58515bd10e421dacc2b25b0a6a9c1
Ahmed Hassan <ahmedh@mathworks.com>
2025-08-10 22:02:37 -0400

    Updates

--- pipelines/generate_jenkins_pipeline.m
+++ pipelines/generate_jenkins_pipeline.m
@@ -1,32 +1,42 @@
 % Copyright 2025 The MathWorks, Inc.
 
-function generate_jenkins_pipeline(workspace, projectRelativePath, matlabInstallationLocation, agentLabel, build_folder)
-    arguments
-        workspace = pwd;
-        projectRelativePath = "";
-        matlabInstallationLocation = "matlab_bin_path";
-        agentLabel = "jenkins_agent_label";
-        build_folder = "_build_"
-    end
-    cp = openProject(strcat(workspace,filesep,projectRelativePath));
+function generate_jenkins_pipeline()
+    workspace = string(getenv('WORKSPACE'));      % Reading Jenkins workspace environment variable
+    supportPackageRoot = string(getenv('MW_SUPPORT_PACKAGE_ROOT'));
+    relativeProjectPath = string(getenv('MW_RELATIVE_PROJECT_PATH'));
+    remoteBuildCacheName = string(getenv('MW_REMOTE_BUILD_CACHE_NAME'));
+    pipelineGenDirectory = string(getenv('MW_PIPELINE_GEN_DIRECTORY'));
+
+    cp = openProject(strcat(workspace,filesep,string(relativeProjectPath)));
     op = padv.pipeline.JenkinsOptions;
-    % op.PipelineArchitecture = "IndependentModelPipelines"; # SingleStage, SerialStages, SerialStagesGroupPerTask
+    op.AgentLabel = "padv_win_agents";
     op.PipelineArchitecture = "IndependentModelPipelines";
     op.GeneratorVersion = 2;
-    op.MatlabInstallationLocation = matlabInstallationLocation;
-    op.AgentLabel = agentLabel;
-    op.GeneratedPipelineDirectory = build_folder;
+    op.SupportPackageRoot = supportPackageRoot;
+    op.GeneratedPipelineDirectory = pipelineGenDirectory;
     op.StopOnStageFailure = true;
     op.RunprocessCommandOptions.GenerateJUnitForProcess = true;
-    op.ReportPath = "$PROJECTROOT$/PA_Results/Report/PadvReport";
-    op.ProjectRelativePath = projectRelativePath;
+    op.ReportPath = "$PROJECTROOT$/PA_Results/Report/ProcessAdvisorReport";
+    op.RelativeProjectPath = relativeProjectPath;
+    op.RemoteBuildCacheName = remoteBuildCacheName;
+    op.CacheFallbackBranches = ["master", "develop"];
 
+    op.ArtifactServiceMode = 'azure_blob';         % network/jfrog/s3/azure_blob
+    % op.NetworkStoragePath = '<Artifactory network storage path>';
+    % op.ArtifactoryUrl = '<JFrog artifactory url>';
+    % op.ArtifactoryRepoName = '<JFrog artifactory repo name>';
+    % op.S3BucketName = '<AWS S3 bucket name>';
+    % op.S3AwsAccessKeyID = '<AWS S3 access key id>';
+    op.AzContainerName = 'padvblobcontainer';
+    % op.RunnerType = "container";          % default/container
+    % op.ImageTag = 'slcicd.azurecr.io/slcheck/padv-ci:r2024b_apr25t_ci_spkg20250802';
+    % op.ImageArgs = "<Docker container arguments>";
+    
     % Docker image settings
     op.UseMatlabPlugin = false;
-    % examples: "matlab", "matlab-batch", "xvfb-run -a matlab", "xvfb-run -a matlab-batch"
-    % op.MatlabLaunchCmd = "xvfb-run -a matlab-batch";
-    op.MatlabLaunchCmd = "matlab-batch";
-    op.MatlabStartupOptions = "";
-    op.AddBatchStartupOption = false;
+    % op.MatlabLaunchCmd = "xvfb-run -a matlab-batch"; 
+    % op.MatlabLaunchCmd = "matlab"; 
+    % op.MatlabStartupOptions = "";
+    % op.AddBatchStartupOption = false;
     padv.pipeline.generatePipeline(op, "CIPipeline");
 end
