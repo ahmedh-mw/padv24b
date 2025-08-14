a6ea48ccfc012b1aa37b3d1306277365b4faeb6d
Ahmed Hassan <ahmedh@mathworks.com>
2025-08-13 22:49:22 -0400

    padv_wsl_agents

--- .github/workflows/generate_github_pipeline.m
+++ .github/workflows/generate_github_pipeline.m
@@ -4,14 +4,14 @@ function generate_github_pipeline()
     op = padv.pipeline.GitHubOptions;
     op.PipelineArchitecture = "IndependentModelPipelines";
     op.GeneratorVersion = 2;
-    op.SupportPackageRoot = "D:/sb/r24b0811/matlab";
-    op.RunnerLabels = "padv_win_agents";
+    op.SupportPackageRoot = "/home/matlab/Documents/MATLAB/SupportPackages/R2024b";
+    op.RunnerLabels = "padv_wsl_agents";
     op.GeneratedPipelineDirectory = "_pipelineGen_";
     op.StopOnStageFailure = true;
     op.RunprocessCommandOptions.GenerateJUnitForProcess = true;
     op.ReportPath = "$PROJECTROOT$/PA_Results/Report/ProcessAdvisorReport";
     % op.RelativeProjectPath = "level1-a/level2/ProcessAdvisorProjectReferenceExample/";
-    op.RemoteBuildCacheName = "GitHub_Project2";
+    op.RemoteBuildCacheName = "GitHub_root";
     op.CacheFallbackBranches = ["master", "main", "develop"];
 
     % We can enhance the vaidation now of the options on the matlab side
@@ -22,14 +22,14 @@ function generate_github_pipeline()
     % op.S3BucketName = '<AWS S3 bucket name>';
     % op.S3AwsAcessKeyID = '<AWS S3 access key id>';
     op.AzContainerName = 'padvblobcontainer';
-    % op.RunnerType = "container";        % default/container
-    % op.ImageTag = 'slcicd.azurecr.io/slcheck/padv-ci:r2024b_apr25t_ci_spkg20250802';
+    op.RunnerType = "container";        % default/container
+    op.ImageTag = 'slcicd.azurecr.io/slcheck/padv-ci:r2024b_apr25t_ci_spkg20250813';
     
     % Docker image settings
     op.UseMatlabPlugin = false;
-    % op.MatlabLaunchCmd = "xvfb-run -a matlab-batch";
-    % op.MatlabStartupOptions = "";
-    % op.AddBatchStartupOption = false;
+    op.MatlabLaunchCmd = "xvfb-run -a matlab-batch";
+    op.MatlabStartupOptions = "";
+    op.AddBatchStartupOption = false;
     
     padv.pipeline.generatePipeline(op, "CIPipeline");
 end
