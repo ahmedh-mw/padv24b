27364226c6c83abf5a8a4d47dddb334df7327054
Ahmed Hassan <ahmedh@mathworks.com>
2026-03-24 17:04:48 -0400

    Enable model maintainability metrics and requirements report in processmodel

--- processmodel.m
+++ processmodel.m
@@ -9,7 +9,7 @@ function processmodel(pm)
     %% Include/Exclude Tasks in processmodel
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-    includeModelMaintainabilityMetricTask = false;
+    includeModelMaintainabilityMetricTask = true;
     includeModelTestingMetricTask = false;
     includeModelStandardsTask = false;
     includeDesignErrorDetectionTask = false;        % Disabled
@@ -28,7 +28,7 @@ function processmodel(pm)
     includeTopProveCodeQuality = false && ~padv.internal.util.isMACA64 && (~isempty(ver('pscodeprover')) || ~isempty(ver('pscodeproverserver')));% Project Level Top-Model code proving
     includeRefCodeInspection = false;
     includeTopCodeInspection = false;
-    includeGenerateRequirementsReport = false;
+    includeGenerateRequirementsReport = true;
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% Define Shared Path Variables
